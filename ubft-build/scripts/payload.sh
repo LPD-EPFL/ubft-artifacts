29d2e40be3b5825ef918b0f7265a123826412e10
Athanasios Xygkis <athanasios.xygkis@epfl.ch>
2022-10-02 15:53:37 +0200

    ubft: add binaries for fig 7 in the payload

--- ubft-build/scripts/payload.sh
+++ ubft-build/scripts/payload.sh
@@ -9,5 +9,7 @@ shopt -s extglob
 rm -rf payload.zip
 rm -v -f binaries/!("placeholder.txt")
 unzip ubft/ubft-apps/binaries.zip -d binaries
+cp ubft/ubft/build/bin/tail-p2p-ping binaries
+cp ubft/ubft/build/bin/tail-cb-ping binaries
 zip -r payload.zip binaries/
 zip -urj payload.zip scripts/deploy/*
