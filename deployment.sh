55573eec6629fca5323f363996118dee9fe91eb1
Athanasios Xygkis <athanasios.xygkis@epfl.ch>
2022-10-03 13:45:28 +0200

    Add deployment script

---
+++ deployment.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+cd "$SCRIPT_DIR"
+
+rm -rf deployment.zip
+zip -r deployment.zip minbft-build/payload.zip
+zip -r deployment.zip ubft-build/payload.zip
+zip -r deployment.zip usig-build/payload.zip
+zip -r deployment.zip experiments
