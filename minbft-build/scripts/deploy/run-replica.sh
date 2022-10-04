83f3b0db6f7ee0baa623ef18098585df71c8f541
Athanasios Xygkis <athanasios.xygkis@epfl.ch>
2022-10-04 14:09:41 +0200

    Minor fixes in ubft-build

--- minbft-build/scripts/deploy/run-replica.sh
+++ minbft-build/scripts/deploy/run-replica.sh
@@ -8,7 +8,7 @@ REPLICA_ID=$1
 CRYPTO=$2 # minbft_ecsda or minbft_noecdsa
 VMA=$3 # vma or nothing
 
-cd "$(pwd)"/binaries/$CRYPTO
+cd "$(pwd)"/binaries/$CRYPTO/minbft
 
 export LD_LIBRARY_PATH="$(pwd)/lib${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}"
 
