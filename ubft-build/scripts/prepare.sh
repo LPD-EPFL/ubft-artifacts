648af3c8a124f37a42acfd636297036dcb1a8283
Athanasios Xygkis <athanasios.xygkis@epfl.ch>
2022-10-12 17:24:29 +0200

    Fix ubft repo url

--- ubft-build/scripts/prepare.sh
+++ ubft-build/scripts/prepare.sh
@@ -4,9 +4,10 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 cd "$SCRIPT_DIR"/..
 
-git clone git@github.com:LPD-EPFL/dory.git ubft
+git clone https://github.com/LPD-EPFL/ubft
+
 cd ubft
-git checkout ubft-replicated-apps
+sed -i 's/"with_mu": False,/"with_mu": True,/g' ubft-apps/conanfile.py
 
 cp -r ../../mu-build/mu/crash-consensus/libgen/prebuilt-lib/include/* crash-consensus/src/include/
 cp -r ../../mu-build/mu/crash-consensus/libgen/prebuilt-lib/lib/* crash-consensus/src/lib/
